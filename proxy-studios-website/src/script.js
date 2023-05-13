5dcc4ebe0c124128cd051083e1987670a21439e0
soli83 <soheil83@gmail.com>
2023-05-14 00:03:05 +0200

    Cleanup
    - Removed redundant files
    - Split up scss

--- proxy-studios-website/src/script.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-    document.getElementById("header").classList.add("active");
-  } else {
-    document.getElementById("header").classList.remove("active");
-  }
-}
